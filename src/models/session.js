b1e7483197e5637f5dc4872730e342d559987a74
Coty Condry <ccondry@cisco.com>
2019-08-08 11:52:08 -0500

    clean up cached/returned session data

--- src/models/session.js
+++ src/models/session.js
@@ -4,11 +4,22 @@ This loads the session.xml file that is created by the dCloud topology
 
 const fs = require('fs')
 const parser = require('./parser')
+const PhoneNumber = require('awesome-phonenumber')
 
-let session = {}
+// list of PSTN country codes for dCloud datacenters
+const countryCodes = {
+  RTP: '+1',
+  SJC: '+1',
+  LON: '+44',
+  SNG: '+65',
+  GC: '+86'
+}
 
 // dcloud session file path
 const sessionFile = process.env.DCLOUD_SESSION_XML || '/dcloud/session.xml'
+
+// cache
+let session = {}
 
 function readSessionFile () {
   // read the dcloud session file and return the contents of the DIDs section
@@ -16,8 +27,33 @@ function readSessionFile () {
     if (err) return console.error(err)
     // parse xml to json object
     const json = parser.xml2js(data)
-    // extract the DIDs array
-    session = json.session
+
+    // determine phone number country code prefix
+    const prefix = countryCodes[json.session.datacenter]
+    // format session DIDs
+    const dids = json.session.dids.did.map(v => {
+      // copy number as-is first
+      let number = v.number
+      // now try to replace it with a nicer phone number
+      try {
+        number = new PhoneNumber(prefix + v.number).getNumber('international')
+      } catch (e) {
+        console.log('error while making nice DIDs object:', e.message)
+      }
+
+      return {
+        name: v.name,
+        number
+      }
+    })
+
+    // update cache - only return session ID, datacenter, and DIDs
+    session = {
+      id: json.session.id,
+      datacenter: json.session.datacenter,
+      dids
+    }
+
   })
 }
 
